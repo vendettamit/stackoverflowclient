2f164793d5d8c072823520eeddc0627459f04be2
vendettamit <vendett.amit@gmail.com>
2015-10-15 17:11:33 -0400

    Fixed issue with rest request getting no-data.

--- src/StackQuestionsClient/ServiceAPI.cs
+++ src/StackQuestionsClient/ServiceAPI.cs
@@ -11,8 +11,8 @@ namespace StackQuestionsClient
     public class ServiceAPI
     {
         private readonly string key = "LdzA8KAXI)POXiHQRw2sBQ((";
-        public static string url = "https://api.stackexchange.com/2.2/questions?&key=LdzA8KAXI)POXiHQRw2sBQ((&order=desc&sort=creation&site=stackoverflow&tagged=csharp,WCF";
-        private static string args = "&key=LdzA8KAXI)POXiHQRw2sBQ((&order=desc&sort=creation&site=stackoverflow&tags=csharp,wcf&filter=!-*f(6t*YrKg4";
+        public static string url = "https://api.stackexchange.com/2.2/questions?&key=LdzA8KAXI)POXiHQRw2sBQ((&order=desc&sort=creation&site=stackoverflow&tagged=csharp&filter=!-*f(6t*YrKg4";
+        private static string args = "";//"&key=LdzA8KAXI)POXiHQRw2sBQ((&order=desc&sort=creation&site=stackoverflow&tags=csharp,wcf&filter=!-*f(6t*YrKg4";
 
         public ServiceAPI()
         {
@@ -22,10 +22,10 @@ namespace StackQuestionsClient
         public async Task<IRestResponse<Rootobject>> GetLatestQuestion()
         {
             var client = new RestClient(url);
-
-            var request = new RestRequest("2.2/questions?&{id}", Method.POST);
+            client.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36";
+            var request = new RestRequest(Method.GET);
             // request.AddParameter("name", "value"); // adds to POST or URL querystring based on Method
-            request.AddUrlSegment("id", args);
+            // request.AddUrlSegment("id", args);
             var response = await client.ExecuteGetTaskAsync<Rootobject>(request);
 
             return response;
